c2973e36a3d760e4bd18848b455c66216c761db0
dpwdec <declankolakowski@gmail.com>
2020-06-15 16:35:04 +0100

    added test for multiple transactions.

--- spec/test_account.py
+++ spec/test_account.py
@@ -15,3 +15,9 @@ class TestAccount(unittest.TestCase):
         account = Account(0)
         account.transact(200)
         self.assertEqual(account.transactions[0], 200)
+    
+    def test_execute_multiple_transactions(self):
+        account = Account(0)
+        account.transact(200)
+        account.transact(-50)
+        self.assertEqual(len(account.transactions), 2)
