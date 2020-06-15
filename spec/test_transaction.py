746defa08ca43123036fce221afaa675839fa6ed
dpwdec <declankolakowski@gmail.com>
2020-06-15 16:55:01 +0100

    date has time format query passed to it when get_formatted_date called.

--- spec/test_transaction.py
+++ spec/test_transaction.py
@@ -13,3 +13,9 @@ class TestTransaction(TestCase):
         date_object = mock.Mock()
         transaction = Transaction(200, date_object)
         self.assertEqual(transaction.date, date_object)
+
+    def test_get_formatted_date(self):
+        date_object = mock.Mock()
+        transaction = Transaction(200, date_object)
+        transaction.get_formatted_date()
+        transaction.date.strftime.assert_called_with("%d/%m/%Y")
--- src/transaction.py
+++ src/transaction.py
@@ -3,3 +3,6 @@ class Transaction():
     def __init__(self, value, date):
         self.value = value
         self.date = date
+
+    def get_formatted_date(self):
+        self.date.strftime("%d/%m/%Y")
