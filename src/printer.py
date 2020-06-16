dea4cc5329785859f5d8350ccc2435e0165d2925
dpwdec <declankolakowski@gmail.com>
2020-06-16 12:10:07 +0100

    refactored debit and credit column formatting into a private method in Printer.

--- src/printer.py
+++ src/printer.py
@@ -10,21 +10,8 @@ class Printer():
             balance_value = "%.2f" % balance
             padding = " " * (7 - len(balance_value))
             balance_col = balance_value + padding
-            input_cols = ""
+            input_cols = self._format_debit_credit_cols(transaction)
             transaction_row = ""
-
-            if(transaction.is_debit()):
-                credit_col = "        || "
-                float_value = "%.2f" % transaction.value
-                padding = " " * (8 - len(float_value))
-                debit_col = f"{float_value}{padding}"
-                input_cols = credit_col + debit_col + "|| "
-            else:
-                debit_col = "        || "
-                float_value = "%.2f" % abs(transaction.value)
-                padding = " " * (8 - len(float_value))
-                credit_col = f"{float_value}{padding}"
-                input_cols = credit_col + "|| " + debit_col
             
             transaction_row = date_col + input_cols + balance_col
             statement += transaction_row
@@ -32,3 +19,20 @@ class Printer():
             balance += transaction.value
 
         return statement
+
+    def _format_debit_credit_cols(self, transaction):
+        input_cols = ""
+        if(transaction.is_debit()):
+            credit_col = "        || "
+            float_value = "%.2f" % transaction.value
+            padding = " " * (8 - len(float_value))
+            debit_col = f"{float_value}{padding}"
+            input_cols = credit_col + debit_col + "|| "
+        else:
+            debit_col = "        || "
+            float_value = "%.2f" % abs(transaction.value)
+            padding = " " * (8 - len(float_value))
+            credit_col = f"{float_value}{padding}"
+            input_cols = credit_col + "|| " + debit_col
+        
+        return input_cols
