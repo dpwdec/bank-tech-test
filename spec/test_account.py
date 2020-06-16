8508523e72c9844c082a2fad4c7661a1f129b139
dpwdec <declankolakowski@gmail.com>
2020-06-16 10:55:02 +0100

    removed statement printing tests from account and added them to printer.

--- spec/test_account.py
+++ spec/test_account.py
@@ -28,28 +28,6 @@ class TestAccount(TestCase):
     def test_transact_calls_transaction_class(self):
         self.account.transact(200)
         self.TransactionClass.assert_called_once_with(200)
-    
-    def test_print_zero_line_statement(self):
-        """
-        If transactions is empty print only account history headers
-        date       || credit  || debit  || balance
-        """
-        self.assertEqual(self.account.print_statement(), "date       || credit  || debit  || balance")
-    
-    def test_print_one_line_statement(self):
-        """
-        If transactions is contains one transaction for 200
-        date       || credit  || debit  || balance
-        14/01/2012 ||         || 200.00 || 0.00
-        """
-        statement =  "date       || credit  || debit  || balance\n14/01/2012 ||         || 200.00 || 0.00"
-        positive_transaction = mock.Mock()
-        positive_transaction.get_formatted_date.return_value = "14/01/2012"
-        positive_transaction.is_debit.return_value = True
-        positive_transaction.transaction_value = 200
-        #self.assertEqual(positive_transaction.transaction_value, 200)
-        self.account.transact(positive_transaction)
-        self.assertEqual(self.account.print_statement(), statement)
 
     def test_returns_balance(self):
         self.assertEqual(self.account.current_balance(), 0)
--- src/printer.py
+++ src/printer.py
@@ -2,3 +2,25 @@ class Printer():
     
     def print_statement(self):
         pass
+
+    # def test_print_zero_line_statement(self):
+    #     """
+    #     If transactions is empty print only account history headers
+    #     date       || credit  || debit  || balance
+    #     """
+    #     self.assertEqual(self.account.print_statement(), "date       || credit  || debit  || balance")
+    
+    # def test_print_one_line_statement(self):
+    #     """
+    #     If transactions is contains one transaction for 200
+    #     date       || credit  || debit  || balance
+    #     14/01/2012 ||         || 200.00 || 0.00
+    #     """
+    #     statement =  "date       || credit  || debit  || balance\n14/01/2012 ||         || 200.00 || 0.00"
+    #     positive_transaction = mock.Mock()
+    #     positive_transaction.get_formatted_date.return_value = "14/01/2012"
+    #     positive_transaction.is_debit.return_value = True
+    #     positive_transaction.transaction_value = 200
+    #     #self.assertEqual(positive_transaction.transaction_value, 200)
+    #     self.account.transact(positive_transaction)
+    #     self.assertEqual(self.account.print_statement(), statement)
